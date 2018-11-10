c651134e59a6dfbbc72b5474ff507919e90e9a2d
antivenomX <43976388+antivenomX@users.noreply.github.com>
2018-11-10 09:22:20 +0800

    Add files via upload

---
+++ wbst.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	<style>
+	 * {
+	 box-sizing : border-box;
+	 }
+	 body {
+	 font-family: Arial;
+	 padding; 10px;
+	 background: #f1f1f1;
+	 }
+	 .header {
+	 padding: 80px;
+	 object-fit: cover;
+	 background-image: url(dota2.jpg);
+	 background-size: fill;
+	 background-repeat: no-repeat;
+	 background-position: 80% 20%
+	 }
+	 .topnav {
+	 	overflow: hidden;
+	 	background-color: #333;
+	 }
+	 .topnav a {
+	 	float: left;
+	 	display: block;
+	 	color: #f2f2f2;
+	 	text-align: center;
+	 	padding: 14px 16px;
+	 	text-decoration: none;
+	 }
+	 .topnav a:hover {
+	 	background-color: #ddd;
+	 	color: black;
+	 }
+	 .leftcolumn {
+	 	float: left;
+	 	width:25%;
+	 }
+	 .rightcolumn{
+	 	float: right;
+	 	width:75%;
+	 	background-color: #f1f1f1;
+	 }
+	 .imge {
+	 	background-color: white;
+	 	width: 100%;
+	 	padding: 20px;
+	 }
+	 .card {
+	 	background-color: white;
+	 	padding: 20px;
+	 	margin-top: 20px;
+	 }
+	 .row:after {
+	 	content: "";
+	 	display: table;
+	 	clear: both;
+	 }
+	 .footer{
+	 	padding:20px;
+	 	text-align: center;
+	 	background: #ddd;
+	 	margin-top: 20px;
+	 }
+	 .txt{
+	 	background-color: white;
+	 	padding: 20px;
+	 	margin-top: 20px;
+	 }
+	</style>
+	</head>
+<body>
+	<div class="header" >
+	</div>
+
+	<div class = "topnav">
+		<a href="wbst.php">Home</a>
+		<a href="#">Contact Info Page</a>
+		<a href="#">Portfolio</a>
+		<a href="#" style="float:right">FAQ</a>
+	</div>
+<div class="row">
+	<div class="rightcolumn">
+		<div class="card">
+			<h2>INFORMATION</h2>
+			<div class="imge"><img src="lolis.jpg" alt="loli" style="height:200px;"></div>
+			<p>2D is life obviously.</p>
+			<p>Post-Modernism means every values/cultures are the same so respect that in my village when you're eight you're ready to mate. Kappa</p>
+		</div>
+	</div>
+	<div class ="leftcolumn">
+		<div class="card">
+			<h2>About Me</h2>
+			<div class="img"><img src="monkahmmm.jpg" alt="monkaHmmm" style="height:200px;"></div>
+			<div class="txt">
+			<p>Something about me... I'm nothing special actually just a normal human-being</p>
+			<p>Age: 19</p>
+			<p>Gender: Male</p> </div>
+		</div>
+		"Gravitation is the power of loneliness attracting each other" -certain someone
+	<div class="card">
+		<h3>Something useless probably...</h3>
+	<div class="imge"><p><img src="dragonloli.png" alt="dragonloli" style="height:200px;"></p></div>
+</div>
+	<div class="card">
+		<h3>Follow Me</h3>
+		<p> ...Jebaited</p>
+	</div>
+</div>
+</div>
+<div class ="footer">
+	<h2>...</h2>
+</div>
+</body>
+
+</html>
